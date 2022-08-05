17ac2145b9d8801d03beb07d99e2cd59717dc806
MathRoda <mouawiahammo65@gmail.com>
2022-08-05 14:38:37 +0200

    initial commit

---
+++ app/src/main/java/com/mathroda/noteme/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mathroda.noteme.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mathroda.noteme.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
