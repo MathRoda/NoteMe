d2633fdfe5cf7285ca085a5b7e83dd5dab85bb1e
MathRoda <mouawiahammo65@gmail.com>
2022-08-05 16:27:36 +0200

    starter project commit - add main screen ( Notes List ) - add note screen ( add/edit)

---
+++ app/src/main/java/com/mathroda/noteme/presentation/note/NoteFragment.kt
@@ -0,0 +1,18 @@
+package com.mathroda.noteme.presentation.note
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mathroda.noteme.R
+class NoteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+}
